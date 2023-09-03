705c4a23c3f2934346690c33b7637f08dd151abf
DaScOrPiO <dunni82@yahoo.com>
2023-09-03 23:32:19 +0100

    FEAT: Add review functionality

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const campgroundSchema = new Schema({
   title: {
@@ -22,6 +23,12 @@ const campgroundSchema = new Schema({
     type: String,
     // required: true,
   },
+  ratings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
