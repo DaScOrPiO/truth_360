3528cc9fd806dcc937407ce543d6ec6ad169a19a
DaScOrPiO <dunni82@yahoo.com>
2024-01-03 16:42:54 -0800

    FEAT: rendered trending movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,14 +5,18 @@ module.exports.showMovies = async (req, res, next) => {
   try {
     const key = process.env.movieKey;
     const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}`;
+    const url2 = `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}`;
     const response = await axios.get(url);
-    const movies = await response.data
-    const data = movies.results
-    if (data.length <= 0) {
+    const response2 = await axios.get(url2);
+    const moviesByCurrentMonth = await response2.data;
+    const movies = await response.data;
+    const data = movies.results;
+    const data2 = moviesByCurrentMonth.results.map((el) => el);
+    if (data.length <= 0 && data2.length <= 0) {
       req.flash("error", "No items to display");
     } else {
-      console.log(data)
-      res.render("Pages/movies/index", { data });
+      console.log(data2);
+      res.render("Pages/movies/index", { data, data2 });
     }
   } catch (err) {
     next(err);
